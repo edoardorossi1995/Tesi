88a06684b82ac719a1f53c7f068731ed076df9e7
BQR <edorossikeyb@gmail.com>
2023-02-21 12:12:16 +0100

    update cartella

---
+++ test_prima_pca/test.py
@@ -0,0 +1,5 @@
+# set conda environment
+import os
+os.environ['CONDA_DEFAULT_ENV'] = 'tesi'
+os.environ['CONDA_PREFIX'] = '/home/andrea/anaconda3/envs/tesi'
+os.environ['CONDA_EXE'] = '/home/andrea/anaconda3/bin/conda'
